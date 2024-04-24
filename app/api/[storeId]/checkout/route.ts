e930e9e0ce2d8f3c0d70c55f3cbbf7e2bbcfcd4c
Brian <briankaine1604@gmail.com>
2024-04-24 22:36:06 +0100

    final-comit

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -7,7 +7,7 @@ interface Products {
   quantity: number;
 }
 
-export const corsHeaders = {
+const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
